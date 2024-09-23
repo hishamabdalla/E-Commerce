8a004cdd559f2b817e6b349216c24ad524b88caf
m-anass <manas8269@gmail.com>
2024-09-23 16:30:39 +0300

    Add UserAddressConfiguration

---
+++ Data/Configurations/UserConfigurations/UserAddressConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using E_Commerce.Models.UserFile;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace E_Commerce.Data.Configurations.UserConfigurations
+{
+    public class UserAddressConfiguration : IEntityTypeConfiguration<UserAddress>
+    {
+        public void Configure(EntityTypeBuilder<UserAddress> builder)
+        {
+            builder.Property(a => a.City)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            builder.Property(a => a.Region)
+                .IsRequired();
+
+            builder.Property(a => a.PostalCode)
+                .IsRequired();
+
+            builder.Property(a => a.PostalCode)
+                .IsRequired();
+
+        }
+    }
+}
