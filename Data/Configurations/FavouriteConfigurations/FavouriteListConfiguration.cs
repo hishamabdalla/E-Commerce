e47d8440e7f1a59d0ce8a34637eb34352691a61b
hishamabdalla <hishvmabdalla@gmail.com>
2024-09-25 00:56:16 +0300

    FavouraiteListConfiguration

---
+++ Data/Configurations/FavouriteConfigurations/FavouriteListConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using E_Commerce.Models.Favourite;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace E_Commerce.Data.Configurations.FavouriteConfiguration
+{
+    public class FavouriteListConfiguration : IEntityTypeConfiguration<FavouriteList>
+    {
+        public void Configure(EntityTypeBuilder<FavouriteList> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.ToTable("FavouriteLists");
+
+            builder.HasMany(f => f.FavouriteListItems).
+                WithOne(f => f.FavouriteList)
+                 .HasForeignKey(i => i.FavouriteListId);
+
+            builder.HasOne(f => f.User)
+                .WithMany(u => u.FavouriteLists)
+                .HasForeignKey(f => f.UserId);
+               
+                
+        }
+    }
+}
