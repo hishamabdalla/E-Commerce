6623dcde8945e4245cc287c15bf0c7fc8676fa88
hishamabdalla <hishvmabdalla@gmail.com>
2024-09-24 14:08:20 +0300

    FavouriteListItemsConfiguration

---
+++ Data/Configurations/FavouriteConfiguration/FavouriteListItemsConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using E_Commerce.Models.Favourite;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace E_Commerce.Data.Configurations.FavouriteConfiguration
+{
+    public class FavouriteListItemsConfiguration : IEntityTypeConfiguration<FavouriteListItems>
+    {
+        public void Configure(EntityTypeBuilder<FavouriteListItems> builder)
+        {
+            builder.HasKey(f => new
+            {
+                f.ProductItemId,
+                f.FavouriteListId
+            }); 
+
+            builder.HasOne(p => p.Product)
+                .WithMany(f => f.FavouriteListItems)
+                .HasForeignKey(f => f.ProductItemId);
+
+            builder.HasOne(p => p.FavouriteList)
+               .WithMany(f => f.FavouriteListItems)
+               .HasForeignKey(f => f.FavouriteListId);
+
+          
+        }
+    }
+}
--- Models/Favourite/FavouriteListItems.cs
+++ Models/Favourite/FavouriteListItems.cs
@@ -4,9 +4,7 @@ namespace E_Commerce.Models.Favourite
 {
     public class FavouriteListItems
     {
-        [ForeignKey("FavouriteList")]
         public int FavouriteListId { get; set; }
-        [ForeignKey("Products")]
         public int ProductItemId { get; set; }
         public virtual Product_Item? Product { get; set; }
         public virtual FavouriteList? FavouriteList { get; set; }
