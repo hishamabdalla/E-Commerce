1a5d4eb98e3c741a3ad0ad9383450cea5b4313a1
m-anass <manas8269@gmail.com>
2024-09-30 23:08:54 +0300

    CRUD on Product

---
+++ E-Commerce/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using E_Commerce.Models.ViewModels;
+using E_Commerce.Models.Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Upsert(int? id)
+        {
+            ProductWithCategorySelectListVM productVM = new()
+            {
+                Product = (id != 0 || id is not null) ? new Product(): new Product(),
+                // categorylist
+            };
+            return View(productVM);
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(ProductWithCategorySelectListVM productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                if(productVM.Product.Id != 0)
+                {
+                    //update
+                }
+                // add
+                // save
+                return RedirectToAction(nameof(Index));
+            }
+            // categorylist
+            return View(productVM);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            // get
+            return View();
+        }
+
+        [HttpPost, ActionName("DeleteBehavior")]
+        public IActionResult DeleteProduct(int id)
+        {
+            // delete
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
