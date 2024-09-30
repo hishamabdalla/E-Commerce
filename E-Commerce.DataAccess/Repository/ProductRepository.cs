4f4f66ab6dc2f52d3614bcf8b4900e5b1900d8bd
m-anass <manas8269@gmail.com>
2024-09-30 22:22:10 +0300

    Create Product Repository

---
+++ E-Commerce.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using E_Commerce.DataAccess.Data;
+using E_Commerce.DataAccessDataAccess.Repository.IRepository;
+using E_Commerce.Models.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.DataAccessDataAccess.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext db;
+
+        public ProductRepository(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public void Update(Product product)
+        {
+            db.Products.Update(product);
+        }
+    }
+}
