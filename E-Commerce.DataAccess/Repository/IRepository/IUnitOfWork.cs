083b2cae413243ec296b00fefc315ec77fd54350
hishamabdalla <hishvmabdalla@gmail.com>
2024-10-01 00:24:03 +0300

    Add Category to IUnitOfWork

--- E-Commerce.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ E-Commerce.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,8 @@ namespace E_Commerce.DataAccessDataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         public IProductRepository Product { get; }
+        public ICategoryRepository Category { get; }
+
         void Save();
     }
 }
