d8043c5a2029b64995e53f63a2f17ff0153dfb45
hishamabdalla <hishvmabdalla@gmail.com>
2024-10-18 10:27:15 +0300

    OrderController

---
+++ E-Commerce/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using E_Commerce.DataAccessDataAccess.Repository.IRepository;
+using E_Commerce.Models.OrderFile;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List <Order> orders=_unitOfWork.Order.GetAll().ToList();
+            return View();
+        }
+
+      
+    }
+}
+
